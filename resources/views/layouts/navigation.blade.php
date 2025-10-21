062f4c0a8dbff0b82ea261d76750e1dfea6f932a
Niclas <niclasreutter@icloud.com>
2025-10-21 23:06:13 +0200

    fix: Remove unnecessary border classes from navigation component

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="border-b border-gray-100 dark:border-gray-700" style="background-color: #00337F;">
+<nav x-data="{ open: false }" style="background-color: #00337F;">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
